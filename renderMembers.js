6397c56c784d689cc533615237a98343cf603e4f
qsysmine <echomanari@gmail.com>
2014-02-20 17:03:32 -0800

    Fixed new Data facepalm

--- renderMembers.js
+++ renderMembers.js
@@ -1,6 +1,6 @@
 (function ($) {
     $('#content').children('a').each(function () {
         var $this = $(this);
-        $this.append("<img src=\"" + $this.attr("data-member") + ".png?cachingDis=" + new Data().getTime() +"\"/>");
+        $this.append("<img src=\"" + $this.attr("data-member") + ".png?cachingDis=" + new Date().getTime() +"\"/>");
     });
 })(jQuery);
