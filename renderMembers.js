dcb07197059f613ab40304e3e0702ad616d0e323
qsysmine <echomanari@gmail.com>
2014-04-06 19:54:56 -0700

    Added Favicon

--- renderMembers.js
+++ renderMembers.js
@@ -1,5 +1,5 @@
 (function ($) {
-    $('#content').children('a').each(function () {
+    $('#content').children('.member').each(function () {
         var $this = $(this);
         $this.append("<img src=\"" + $this.attr("data-member") + ".png?cachingDis=" + new Date().getTime() +"\"/>");
     });
