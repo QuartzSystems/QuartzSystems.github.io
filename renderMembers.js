0eee3f8d7387498ed5978ba33895ed54cf484c73
qsysmine <echomanari@gmail.com>
2014-04-13 07:09:30 -0700

    Redone
    Redid the Quartz Systems website, it is now Quartz Systems video
    central.

--- renderMembers.js
+++
@@ -1,6 +0,0 @@
-(function ($) {
-    $('#content').children('.member').each(function () {
-        var $this = $(this);
-        $this.append("<img src=\"" + $this.attr("data-member") + ".png?cachingDis=" + new Date().getTime() +"\"/>");
-    });
-})(jQuery);
